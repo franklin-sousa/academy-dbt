e3089f43c3e0a544773e728978bfa87771228692
Franklin Machado <franklin.sousa@equatorialenergia.com.br>
2024-03-24 03:22:55 +0000

    Criada stage cartao_credito

---
+++ models/staging/sap/stg_cartao_credito.sql
@@ -0,0 +1,20 @@
+with
+    fonte_cartao_credito as (
+        select
+             cast(CREDITCARDID as int) as pk_cartao_credito
+            ,cast(CARDTYPE as string) as bandeira
+            ,cast(CARDNUMBER as int) as cartao_numero
+            ,cast(EXPMONTH as int) as mes_validade
+            ,cast(EXPYEAR as int) as ano_validade
+            --,cast(MODIFIEDDATE as string) as data_dados
+        from {{ source('sap', 'creditcard') }}
+    )
+,renomear as (
+    select 
+*
+     
+from fonte_cartao_credito
+)
+
+select *
+from renomear
